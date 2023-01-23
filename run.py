68565ed878186f4a270b4aacd9b8763311ab866f
cedric <cedric.maigrot@ose-engineering.fr>
2023-01-23 13:23:15 +0100

    feat: Wamiz data collect

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ import os
 
 from fci import scrapper as fci
 from laspa import scrapper as laspa
+from wamiz import scrapper as wamiz
 
 if not os.path.exists("data"):
     os.makedirs("data")
 
 data = fci.run()
-# data = laspa.run()
+data = laspa.run()
+data = wamiz.run()
